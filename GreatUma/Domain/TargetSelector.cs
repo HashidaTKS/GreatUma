bafef086aa564572a847f9a989d6a54065cf64cc
Takashi Hashida <hashida@clear-code.com>
2024-08-27 23:49:37 +0900

    Add target selector sample

---
+++ GreatUma/Domain/TargetSelector.cs
@@ -0,0 +1,35 @@
+﻿using GreatUma.Utils;
+using System.Collections.Generic;
+using System.Linq;
+using GreatUma.Models;
+using GreatUma.Model;
+
+namespace GreatUma.Domain
+{
+    internal class TargetSelector()
+    {
+        private static double TargetPlaceOdds = 1.1; 
+
+        internal IEnumerable<HorseAndOddsCondition> GetTargets(RaceData raceData, List<OddsDatum> oddsData, List<HorseDatum> horseData)
+        {
+            foreach(var oddsDatum in oddsData.Where(_ => _.HighOdds < TargetPlaceOdds))
+            {
+                if (oddsDatum.HorseData.Count != 1)
+                {
+                    // 複勝のオッズなので、馬は一頭。（馬連なら二頭、三連複なら三頭になる。）
+                    continue;
+                }
+                var horseNumberForOdds = oddsDatum.HorseData.FirstOrDefault().Number;
+                var horseDatum = horseData.FirstOrDefault(_ => _.Number == horseNumberForOdds);
+                if(horseDatum != null)
+                {
+                    yield return new HorseAndOddsCondition()
+                    {
+                        StartTime = raceData.StartTime, 
+                    };
+                }
+            }
+
+        }
+    }
+}
